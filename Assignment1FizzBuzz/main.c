0d285cf71be74421b728f0d48f8ff1ae4bb91635
Mike Reining <emailmike@gmail.com>
2014-11-03 12:06:54 -0800

    FizzBuzz from 1 to 100 with While and For Loop

--- Assignment1FizzBuzz/main.c
+++ Assignment1FizzBuzz/main.c
@@ -8,8 +8,49 @@
 
 #include <stdio.h>
 
+// For Loop example
+
+void printForLoop () {
+    for (int number = 1; number <=100; number++) { // for Numbers 1 to 100
+        if (number %3 ==0 && number %5 ==0) { // If multiple of 3 and 5
+            printf("FizzBuzz\n");
+        } else if (number %3 ==0) { // If multiple of 3
+            printf("Fizz\n");
+        } else if (number %5 == 0) { // If multiple of 5
+            printf("Buzz\n");
+        } else {
+            printf("%d\n",number); // else just print number
+        }
+    }
+}
+
+void printWhileLoop (int number) {
+    while (number <101) {
+        if (number %3 ==0 && number %5 ==0) {
+            printf("FizzBuzz\n");
+            number++;
+        } else if (number %3 ==0){
+            printf("Fizz\n");
+            number++;
+        } else if (number %5 ==0){
+            printf("Buzz\n");
+            number++;
+        } else {
+            printf("%d\n",number);
+            number++;
+            }
+    }
+}
+
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    printf("Assignment 1 completed with For Loop below:\n");
+    
+    printForLoop(); // execute for loop
+
+    printf("\n Assignment 1 completed with While Loop below:\n");
+    
+    printWhileLoop(1);
+    
     return 0;
 }
